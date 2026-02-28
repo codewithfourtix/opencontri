b4e4a554f97407e78b7f1e49b6fac3af11711682
Ali Zulfiqar <codewithfourtix@gmail.com>
2026-02-28 22:29:33 +0500

    adding google analytics link

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'GitHub Contribution Analyzer',
@@ -8,6 +9,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-5G2XF8MBV1"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-5G2XF8MBV1');
+            `,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
